6cd4bbd7090097365267c2f6ff6eda36a56702e6
taraneh <taraneh.haeri@gmail.com>
2023-09-11 21:39:53 +0330

    optimize mobile styles

--- src/components/customer/login/Login.tsx
+++ src/components/customer/login/Login.tsx
@@ -54,7 +54,7 @@ export default function Login() {
           />
           <Button
             disabled={false}
-            classname="bg-slate-600 text-white  grid place-content-center px-2 py-1 md:px-4 md:py-2 rounded-xl hover:bg-slate-800"
+            classname="bg-slate-600 text-white grid place-content-center px-4 py-2 rounded-xl hover:bg-slate-800"
             onClick={handleLogin}
           >
             login
